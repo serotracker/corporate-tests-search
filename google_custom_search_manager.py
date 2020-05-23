664ff085ccd951583dced733177267a099f3c89b
Simona Rocco <serocco@uwaterloo.ca>
2020-05-23 15:44:48 -0400

    refactoring & clean up/organization

---
+++ google_custom_search_manager.py
@@ -0,0 +1,96 @@
+import os
+
+import pandas as pd
+
+from googleapiclient.discovery import build
+
+
+def create_search_engine():
+    # Extract api key
+    api_key = os.getenv('CUSTOM_SEARCH_API_KEY')
+
+    # Build custom search instance
+    custom_search = build("customsearch", "v1",
+                          developerKey=api_key)
+    return custom_search
+
+
+def _get_search_results_for_company(company, engine_id, search_engine, **kwargs):
+    # Submit API request and extract search results from total response
+    query = '{} orders antibody tests for employees'.format(company)
+    params = dict(q=query, cx=engine_id, **kwargs)
+    response = search_engine.cse().list(**params).execute()
+    search_results = response['items']
+    print(search_results)
+    exit()
+    return search_results
+
+
+def _get_formatted_record_from_results(result, company):
+    # Extract metatags
+    metatags = result['pagemap']['metatags'][0]
+
+    # Extract source type
+    source_type = metatags.get('og:type', 'Not Available')
+
+    # Extract site name
+    site_name = metatags.get('og:site_name', 'Not Available')
+
+    # Extract URL
+    url = metatags.get('og:url', result['link'])
+
+    # Extract title
+    title = metatags.get('og:title', result['title'])
+
+    # Extract published date
+    try:
+        published_date = metatags['article:published_time']
+    except KeyError:
+        try:
+            published_date = metatags['datepublished']
+        except KeyError:
+            try:
+                published_date = metatags['date']
+            except KeyError:
+                published_date = 'Not Available'
+
+    # Extract text preview
+    text_preview = result.get('snippet', 'Not Available')
+
+    # Create dictionary of data for row in df
+    data = {'COMPANY_NAME': company,
+            'SOURCE_TYPE': source_type,
+            'SITE_NAME': site_name,
+            'URL': url,
+            'TITLE': title,
+            'PUBLISHED_DATE': published_date,
+            'TEXT_PREVIEW': text_preview}
+    return data
+
+
+def run_search_across_companies(company_names, search_engine):
+    # Extract id of search engine to use
+    search_engine_id = os.getenv('CSE_ID')
+
+    # Set parameter to only get search results from March 1, 2020 to date in environment variable if present
+    sort_by_date = 'date:r:20200301:'
+    if "LIMIT_DATE" in os.environ:
+        limit_date = os.getenv("LIMIT_DATE")
+        sort_by_date += str(limit_date)
+
+    # Loop through companies and create results output for each company
+    results_list = []
+    for company in company_names:
+        # Get all results for specific company search
+        results_for_company =\
+            _get_search_results_for_company(company, search_engine_id, search_engine,
+                                            sort=sort_by_date, exactTerms=company)
+        # Loop through result and extract each into a row of the final results df for the company
+        for result in results_for_company:
+            # Extract relevant information from result into nicely formatted recrdo
+            formatted_record = _get_formatted_record_from_results(result, company)
+
+            # Add formatted record to over list of records
+            results_list.append(formatted_record)
+    results_df = pd.DataFrame(results_list)
+    return results_df
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import os
 from datetime import datetime
 
 import pandas as pd
+
 from google_services_manager import GoogleServicesManager
-from googleapiclient.discovery import build
+from google_custom_search_manager import create_search_engine, run_search_across_companies
 
 
 def _create_dummy_input_csv():
@@ -17,103 +17,30 @@ def main():
     # Create dummy input csv
     _create_dummy_input_csv()
 
-    # Create google services manager obj to control uploading csvs to Google Drive
-    google = GoogleServicesManager()
+    # Get custom search engine instance
+    custom_search = create_search_engine()
 
-    # Extract api key and cse engine ID env variables
-    api_key = os.getenv('CUSTOM_SEARCH_API_KEY')
-    search_engine_id = os.getenv('CSE_ID')
-
-    # Build custom search instance
-    custom_search = build("customsearch", "v1",
-                    developerKey=api_key)
-
-    # Set query parameter to only get search results from March 1, 2020 to present
-    sort_by_date = 'date:r:20200301:'
-
-    # Loop through companies in input csv and create results output for each company
+    # Run search on all companies and get one df of all search results
     input_df = pd.read_csv('Dummy test.csv')
     companies = input_df['COMPANY_NAME'].tolist()
-    for company in companies:
-        results_for_company_list = []
+    results_df = run_search_across_companies(companies, custom_search)
 
-        # Submit API request and extract search results from total response
-        query = '{} orders antibody tests for employees'.format(company)
-        params = dict(q=query, cx=search_engine_id, sort=sort_by_date, num=10)
-        response = custom_search.cse().list(**params).execute()
-        search_results = response['items']
+    # Set date retrieved column to be current datetime
+    date_retrieved = datetime.now()
+    date_retrieved_day = date_retrieved.day
+    date_retrieved_month = date_retrieved.month
+    date_retrieved_year = date_retrieved.year
+    results_df['DATE_RETRIVED'] = date_retrieved
 
-        # Loop through result and extract each into a row of the final results df for the company
-        for result in search_results:
-            metatags = result['pagemap']['metatags'][0]
+    # Create csv name based on date
+    csv_name = '{}-{}-{} corporate search results.csv'.format(date_retrieved_year,
+                                                              date_retrieved_month,
+                                                              date_retrieved_day)
+    results_df.to_csv(csv_name, index=False)
 
-            # Extract source type
-            try:
-                source_type = metatags['og:type']
-            except KeyError:
-                source_type = 'Not Available'
-
-            # Extract site name
-            try:
-                site_name = metatags['og:site_name']
-            except KeyError:
-                site_name = 'Not Available'
-
-            # Extract URL
-            try:
-                url = metatags['og:url']
-            except KeyError:
-                url = result['link']
-
-            # Extract title
-            try:
-                title = metatags['og:title']
-            except KeyError:
-                title = result['title']
-
-            # Extract published date
-            try:
-                published_date = metatags['article:published_time']
-            except KeyError:
-                try:
-                    published_date = metatags['datepublished']
-                except KeyError:
-                    try:
-                        published_date = metatags['date']
-                    except KeyError:
-                        published_date = 'Not Available'
-
-            # Extract text preview
-            try:
-                text_preview = result['snippet']
-            except KeyError:
-                text_preview = 'Not Available'
-
-            # Set date retrieved field to be current datetime
-            date_retrieved = datetime.now()
-            date_retrieved_day = date_retrieved.day
-            date_retrieved_month = date_retrieved.month
-            date_retrieved_year = date_retrieved.year
-
-            # Create dictionary of data for row in df
-            data = {'COMPANY_NAME': company,
-                    'SOURCE_TYPE': source_type,
-                    'SITE_NAME': site_name,
-                    'URL': url,
-                    'TITLE': title,
-                    'PUBLISHED_DATE': published_date,
-                    'TEXT_PREVIEW': text_preview,
-                    'DATE_RETRIEVED': date_retrieved}
-            results_for_company_list.append(data)
-        results_for_company_df = pd.DataFrame(results_for_company_list)
-
-        # Create csv name based on company and date
-        csv_name = '{}-{}-{} results for {}.csv'.format(date_retrieved_year,
-                                                        date_retrieved_month,
-                                                        date_retrieved_day,
-                                                        company)
-        results_for_company_df.to_csv(csv_name, index=False)
-        google.upload_file_to_drive(csv_name)
+    # Create google services manager obj to control uploading csvs to Google Drive
+    google = GoogleServicesManager()
+    google.upload_file_to_drive(csv_name)
     return
 
 
