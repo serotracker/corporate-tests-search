309ca37bc1441be0d55dc784101c0317716d56c2
Austin Atmaja <austin.atmaja@gmail.com>
2020-06-15 23:21:27 -0400

    filter out <a/> tags that aren't in the article body from text scraping results

--- utils/web-scraper/web_scraper.py
+++ utils/web-scraper/web_scraper.py
@@ -2,14 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def main():
-    url = 'https://toronto.ctvnews.ca/ontario-government-asked-to-take-over-woodbridge-nursing-home-after-18-' \
-          'residents-taken-to-hospital-1.4962347?cache=yes%3Fot%3DAjaxLayout%3FautoPlay%3Dtrue'
+def scrape(url, output_file_path="scraped_website"):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'lxml')
-    with open("scraped_website.html", "w") as f:
+
+    # Filter out hyperlinks (<a/> tags) that are not enclosed by regular text (parent element = <p/> tag)
+    tags_to_discard = filter(lambda x: x.parent.name != 'p', soup.select("a"))
+    for tag in tags_to_discard:
+        tag.extract()
+
+    # Get text and save it to a txt file
+    with open(f"{output_file_path}.txt", "w") as f:
         f.write(soup.get_text(strip=True))
     return
+
+
+def main():
+    url = "https://www.companynewshq.com/company-news/food-and-drink-company-news/tyson-foods-inc-releases-results-from-covid-19-testing-at-berry-street-facility-in-springdale-ar/"
+    scrape(url)
 
 
 if __name__ == '__main__':
