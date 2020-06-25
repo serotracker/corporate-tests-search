8a12898e7ac2ad582e8fa4bce66d6f61f14408a0
Austin Atmaja <austin.atmaja@gmail.com>
2020-06-24 20:38:58 -0400

    Setup csv processing script

--- utils/web-scraper/web_scraper.py
+++ utils/web-scraper/web_scraper.py
@@ -1,8 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+import argparse
+import os
 
 
-def scrape(url, output_file_path="scraped_website"):
+def scrape(url, output_file_path=None):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'lxml')
 
@@ -11,15 +14,44 @@ def scrape(url, output_file_path="scraped_website"):
     for tag in tags_to_discard:
         tag.extract()
 
+    return_text = soup.get_text(strip=True)
+
     # Get text and save it to a txt file
     with open(f"{output_file_path}.txt", "w") as f:
-        f.write(soup.get_text(strip=True))
-    return
+        f.write(return_text)
+    return return_text
+
+def score_text(text, company_name):
+    pass
+
+def process_csv_results(csv_file, write_to_csv=True, dump_txts=True):
+    print(csv_file, write_to_csv, dump_txts)
+    # If dump_txts = true, create a 'results' folder
+    if dump_txts:
+        if not os.path.exists("results"):
+            os.makedirs("results")
+    # Open csv file
+    with open(csv_file, 'r') as f:
+        csv_obj = csv.reader(f)
+        for idx, row in enumerate(csv_obj):
+            if idx > 0:
+                print(row)
+    # Iterate through each row (after the first)
+        # Get the url
+        # call scrape(url) and get the text
+        # if dump_txts, save it to a txt file
+        # if write_to_csv, call score_text(text, company_name) and write to csv
+    # Save the csv
 
 
 def main():
-    url = "https://www.companynewshq.com/company-news/food-and-drink-company-news/tyson-foods-inc-releases-results-from-covid-19-testing-at-berry-street-facility-in-springdale-ar/"
-    scrape(url)
+    parser = argparse.ArgumentParser(description='score relevance of results in a provided csv file')
+    parser.add_argument("csv_file", help="csv file to process")
+    parser.add_argument("--write_to_csv", help="write results to csv or not", action='store_true')
+    parser.add_argument("--dump_txts", help="save scraped text or not", action='store_true')
+
+    args = parser.parse_args()
+    process_csv_results(args.csv_file, args.write_to_csv, args.dump_txts)
 
 
 if __name__ == '__main__':
