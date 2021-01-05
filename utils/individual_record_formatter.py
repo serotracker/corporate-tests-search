c8a135ae49d5dc4b6c85f05c9a5c747106c8f7b6
simonarocco <serocco@uwaterloo.ca>
2021-01-04 22:02:34 -0500

    changing query search terms

--- utils/individual_record_formatter.py
+++ utils/individual_record_formatter.py
@@ -36,7 +36,6 @@ def _get_published_date(meta, snippet):
 
 
 def get_formatted_record_from_results(result, company):
-    print(result)
     try:
         # Extract metatags
         metatags = result['pagemap']['metatags'][0]
@@ -79,13 +78,17 @@ def get_formatted_record_from_results(result, company):
                     'IS_ARTICLE_GOOD': 0,
                     'PUBLISHED_DATE': 'N/A'}
         except KeyError:
-            data = {'COMPANY_NAME': company,
-                    'SOURCE_TYPE': 'N/A',
-                    'URL': result['link'],
-                    'TITLE': result['title'],
-                    'TEXT_PREVIEW': 'N/A',
-                    'OPENED_ARTICLE': 0,
-                    'COMPANY_NAME_IN_TEXT': 0,
-                    'IS_ARTICLE_GOOD': 0,
-                    'PUBLISHED_DATE': 'N/A'}
+            # If still no results, just return empty dict
+            try:
+                data = {'COMPANY_NAME': company,
+                        'SOURCE_TYPE': 'N/A',
+                        'URL': result['link'],
+                        'TITLE': result['title'],
+                        'TEXT_PREVIEW': 'N/A',
+                        'OPENED_ARTICLE': 0,
+                        'COMPANY_NAME_IN_TEXT': 0,
+                        'IS_ARTICLE_GOOD': 0,
+                        'PUBLISHED_DATE': 'N/A'}
+            except KeyError:
+                data = {}
     return data
